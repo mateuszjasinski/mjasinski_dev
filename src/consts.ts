029cfc2b3a1ea6c0f16ee8444d18937055b97e14
mateuszjasinski <matij95@gmail.com>
2026-08-21 14:24:48 +0200

    feat: new article + ping on hex

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Mateusz Jasinski';
-export const SITE_DESCRIPTION = 'Mateusz Jasiński dev blog';
+export const SITE_DESCRIPTION = 'Nine years across web development, and running. Building software with curiosity, precision, and endurance.';
